28179934e154a3188851149223538c07d7517392
Bronson Holden <dev@bronsonholden.com>
2022-10-31 07:45:57 -0500

    Search style improvements and query clearing

--- app/javascript/controllers/remarks_search_controller.js
+++ app/javascript/controllers/remarks_search_controller.js
@@ -2,13 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="remarks-search"
 export default class extends Controller {
-  static targets = ["form", "input"]
+  static targets = ["form", "input", "clear"]
 
   connect() {
     const params = (new URL(document.location)).searchParams
     const searchTerm = params.get("search")
     if (searchTerm) {
       this.inputTarget.value = decodeURIComponent(searchTerm)
+      this.clearTarget.classList.remove("hidden")
+    } else {
+      this.clearTarget.classList.add("hidden")
     }
   }
 
@@ -18,11 +21,18 @@ export default class extends Controller {
       const url = new URL(document.location)
       if (this.inputTarget.value) {
         url.searchParams.set("search", encodeURIComponent(this.inputTarget.value))
+        this.clearTarget.classList.remove("hidden")
       } else {
         url.searchParams.delete("search")
+        this.clearTarget.classList.add("hidden")
       }
       history.pushState(null, "", url.toString())
       this.formTarget.requestSubmit()
     }, 300)
+  }
+
+  clear() {
+    this.inputTarget.value = ""
+    this.search()
   }
 }
