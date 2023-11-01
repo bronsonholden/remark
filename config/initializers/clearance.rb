12af9b41b48c394085a835b5c2ffeb587d6d275d
Bronson Holden <dev@bronsonholden.com>
2023-11-01 15:40:18 -0700

    Add `cookie_domain` option for Clearance

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,7 @@
 Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "noreply@remark.social"
+  config.cookie_domain = Rails.env.development? ? "localhost" : ".remark.social"
   config.rotate_csrf_on_sign_in = true
   config.secure_cookie = true
   config.httponly = true
