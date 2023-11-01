6d06bbf73e81c1017c409e231e1d1e4733c848df
Bronson Holden <dev@bronsonholden.com>
2023-11-01 15:43:02 -0700

    Secure cookies only in production

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -3,7 +3,7 @@ Clearance.configure do |config|
   config.mailer_sender = "noreply@remark.social"
   config.cookie_domain = Rails.env.development? ? "localhost" : ".remark.social"
   config.rotate_csrf_on_sign_in = true
-  config.secure_cookie = true
+  config.secure_cookie = Rails.env.production?
   config.httponly = true
   config.redirect_url = "/feed"
 end
